ddf0a559f557cbb7932d796734479abc157e90a1
Willscreation <row654321@gmail.com>
2025-03-26 14:10:50 +0530

    new

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Styles/Navbarstyle.css'
+import logo from '../../imgs/logo.png'
 
 const Navbar = () => {
   return (
     <div>
     <nav className="Navbar">
-      <div className="Logo"><h1 className='heading'>SnapRoom</h1></div>
+      <div className="Logo">
+        <img className='logoimg' src={logo} alt="" />
+        {/* <h1 className='heading'>SnapRoom</h1> */}
+      </div>
       <ul className="nav-links">
         <li>
           <Link to="/" className="home_link">Home</Link>
